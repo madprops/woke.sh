5ef4ff41fc2dca9b6462097863bc3dc139227350
madprops <easywest@protonmail.com>
2020-02-20 23:10:24 -0600

    Ugh

--- woke.sh
+++ woke.sh
@@ -35,7 +35,7 @@ for (( i=${#sorted_dates[@]}-1 ; i>=0; i-- )); do
     diff=$((sorted_dates[i] - sorted_dates[i - 1]))
     diff2=$((sorted_dates[i - 1] - sorted_dates[i - 2]))
     
-    if [ "$diff" -gt "$gap" ]; then
+    if [ "$diff" -gt "$gap" ] && [ "$diff2" -gt "$gap" ]; then
         sdate=$(date --date @${sorted_dates[i]} +"%r")
         diff3=$((current_date - sorted_dates[i]))
         hours_ago=$(echo "scale=2; ${diff3}/3600" | bc)
